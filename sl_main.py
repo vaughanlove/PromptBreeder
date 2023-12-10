22030439c26178afbd601ef815459b7993eeed1f
Vaughan <vaughanlove1@gmail.com>
2023-12-10 12:29:59 -0700

    disable button if pop == 0

--- sl_main.py
+++ sl_main.py
@@ -132,7 +132,7 @@ mp_selected_rows, ts_selected_rows  = dataframe_with_selections(mp_df, ts_df)
 st.session_state.size = len(mp_selected_rows) * len(ts_selected_rows)
 st.session_state.calls = (st.session_state.size*st.session_state.evals + st.session_state.size // 2)*st.session_state.generations
 
-second_button = st.button(f"run for {st.session_state.generations} generations")
+second_button = st.button(f"run for {st.session_state.generations} generations", disabled=(not (st.session_state.size > 0)))
 if second_button:
     st.session_state.population = create_population(tp_set=ts_selected_rows['0'].tolist(), mutator_set=mp_selected_rows['0'].tolist(), problem_description=problem_description)
     st.session_state.size = st.session_state.population.size
